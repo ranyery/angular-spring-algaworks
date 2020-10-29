11f11ab1099531e95cce260a12b91500ec3c7081
ranyery <ranyerysc@gmail.com>
2020-10-28 23:11:14 -0300

    feat(CategoriaController): adiciona novos métodos

--- algamoney-api/src/main/java/com/algamoney/api/controller/CategoriaController.java
+++ algamoney-api/src/main/java/com/algamoney/api/controller/CategoriaController.java
@@ -1,11 +1,19 @@
 package com.algamoney.api.controller;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.algamoney.api.model.Categoria;
 import com.algamoney.api.repository.CategoriaRepository;
@@ -18,8 +26,33 @@ public class CategoriaController {
 	private CategoriaRepository categoriaRepository;
 	
 	@GetMapping
-	public List<Categoria> listar() {
-		return categoriaRepository.findAll();
+	public ResponseEntity<?> listar() {
+		List<Categoria> categorias = categoriaRepository.findAll();
+		
+		return categorias.isEmpty()
+				? ResponseEntity.noContent().build()
+				: ResponseEntity.ok(categorias);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+			.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
+		if (!categoriaRepository.existsById(codigo)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		Categoria categoria = categoriaRepository.findById(codigo).get();
+		return ResponseEntity.ok(categoria);
 	}
 
 }
